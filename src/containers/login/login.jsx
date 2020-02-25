c1b50b79787fb0be8b49c7e6a3b86207ebfdd6ca
qikaigao <qikaigao2017@gmail.com>
2020-02-25 15:06:42 -0800

    antd and router

---
+++ src/containers/login/login.jsx
@@ -0,0 +1,15 @@
+/*
+登陆 路由组件
+ */
+
+import React,{Component} from "react";
+
+export default class Login extends Component{
+
+    render(){
+        return (
+            <div>Login</div>
+        );
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+/*
+入口 JS
+
+ */
+
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {HashRouter, Route,Switch} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Register from "./containers/register/register";
+import Login from "./containers/login/login";
+import Main from "./containers/main/main";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render((
+    <HashRouter>
+        <Switch>
+            <Route path='/register'><Register/></Route>
+            <Route path='/login'><Login/></Route>
+            <Route><Main/></Route>
+        </Switch>
+    </HashRouter>
+), document.getElementById('root'));
